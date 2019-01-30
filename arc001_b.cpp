2af2cf343c2631ae27d6adff3d7c70a868f72550
kyouhn <kzk0802kazuki@gmail.com>
2019-01-30 22:18:30 +0900

    ARC001のB問題できた

---
+++ arc001_b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int  main(){
+    int A,B;
+    cin >> A >> B;
+    int ans = A - B;
+    while(ans != 0){
+        if(ans > 0){
+            cout << ans << endl;
+        }else{
+            if()
+        }
+    }
+}
